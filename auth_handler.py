ea74d6b5c4e03884d4d61b0d72dba6b35e4944ba
Fe0fan0v <feofanov@yandexlyceum.ru>
2021-10-29 09:51:29 +0500

    jwt

--- auth_handler.py
+++ auth_handler.py
@@ -2,8 +2,9 @@ from datetime import datetime, timedelta
 import jwt
 from typing import Optional
 from os import getenv
+import uuid
 
-JWT_SECRET = getenv('JWT_SECRET')
+JWT_SECRET = getenv('API_KEY')
 JWT_ALGORITHM = getenv('JWT_ALGORITHM')
 
 
@@ -14,16 +15,13 @@ def sign_jwt(email: str) -> str:
         'expires': (datetime.utcnow() + timedelta(days=14)).strftime('%d/%m/%Y %H:%M')
     }
     token = jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
-    return token
+    return str(token)
 
 
 def decode_jwt(token: str) -> Optional:
     # декодирование токена
-    try:
-        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
-        if datetime.strptime(decoded_token['expires'], '%d/%m/%Y %H:%M') >= datetime.utcnow():
-            return decoded_token
-        else:
-            return None
-    except Exception as e:
-        return {'error': e}
+    decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    if datetime.strptime(decoded_token['expires'], '%d/%m/%Y %H:%M') >= datetime.utcnow():
+        return decoded_token
+    else:
+        return None
